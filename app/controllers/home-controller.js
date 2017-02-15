e17277f1cecaf088f56d36e9710bf10a852a38c5
Todor Georgiev <todorgeorgiev96@gmail.com>
2017-02-15 20:45:24 +0200

    TC_API injected to controllers

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = ({}) => {
+module.exports = ({ /* TC_API no need to use it here but you still have access to it */ }) => {
     return {
         getHomePage(req, res) {
            return res.render('./../index.html');
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,13 +3,13 @@
 const path = require('path'),
     fs = require('fs');
 
-module.exports = ({}) => {
+module.exports = ({ TC_API }) => {
     let controllers = [];
 
     fs.readdirSync(__dirname)
         .filter(x => x.includes('-controller'))
         .forEach(file => {
-            let currentController = require(path.join(__dirname, file))({});
+            let currentController = require(path.join(__dirname, file))({ TC_API });
 
             let controllerName = file.substring(0, file.indexOf('-controller'));
 
